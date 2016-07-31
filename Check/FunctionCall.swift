ef493b86317d5169b00788efb9020e60d75b9678
Chris Howell <chris.kevin.howell@gmail.com>
2016-07-31 17:12:34 +0100

    Add Check Xcode project.

---
+++ Check/FunctionCall.swift
@@ -0,0 +1,36 @@
+//
+//  FunctionCall.swift
+//  Check
+//
+//  Created by Chris Howell on 29/07/2016.
+//  Copyright © 2016 Chris Howell. All rights reserved.
+//
+
+import Foundation
+
+struct FunctionCall {
+    /// The name of the function
+    let name: String
+    
+    /// The arguments passed in to the function
+    let args: [Any]?
+}
+
+extension FunctionCall {
+    
+    /**
+     Verifies whether an argument has been provided.
+     
+     - parameter argument: The argument to check for.
+     
+     - returns: Whether that argument was used in this `FunctionCall`
+     */
+    func verifyArgument<T: Equatable>(argument: T) -> Bool {
+        guard let args = args else { return false }
+        return args.contains {
+            guard let typed = $0 as? T else { return false }
+            return typed == argument
+        }
+    }
+    
+}
---
+++ Check/Mock.swift
@@ -0,0 +1,85 @@
+//
+//  Mock.swift
+//  Check
+//
+//  Created by Chris Howell on 29/07/2016.
+//  Copyright © 2016 Chris Howell. All rights reserved.
+//
+
+import Foundation
+
+class Mock {
+    
+    /// All of the calls received by this Mock
+    private(set) var calls: [FunctionCall] = []
+    
+    /// All of the responses that this mock should return
+    /// "methodName" : response
+    var responses: [String : Any] = [:]
+    
+    /**
+     Stubs a response and tracks the method call.
+     
+     - parameter functionName: The function name to track, usually not provided
+     - parameter args:         The arguments to track
+     
+     - returns: The stubbed response to return
+     */
+    func stub<T>(functionName: String = #function, args: [Any]? = nil) -> T? {
+        let shortFunctionName = functionName.componentsSeparatedByString("(").first!
+        calls.append(FunctionCall(name: shortFunctionName, args: args))
+        return responses[shortFunctionName] as? T
+    }
+    
+    /**
+     Tracks the method call without providing a stubbed response.
+     
+     - parameter functionName: The function name to track, usually not provided
+     - parameter args:         The arguments to track
+     */
+    func spy(functionName: String = #function, args: [Any]? = nil) {
+        let shortFunctionName = functionName.componentsSeparatedByString("(").first!
+        calls.append(FunctionCall(name: shortFunctionName, args: args))
+    }
+    
+}
+
+extension Mock {
+    
+    /**
+     Returns a particular function call if it exists.
+     
+     - parameter name: The name of the function call.
+     
+     - returns: An optional function call
+     */
+    func fetchCall(named name: String) -> FunctionCall? {
+        return calls.filter { $0.name == name }.first
+    }
+    
+    /**
+     Verifies whether a particular function was called.
+     
+     - parameter name:       The name of the function.
+     - parameter occurances: The number of times that the function should have been called.
+     
+     - returns: Whether the function was called.
+     */
+    func verifyMethodCall(named name: String, occurances: Int = 1) -> Bool {
+        return calls.filter { $0.name == name }.count == occurances
+    }
+    
+    /**
+     Verifies whether an argument was used within a method call.
+     
+     - parameter method:   The method to check the argument against.
+     - parameter argument: The argument to check against.
+     
+     - returns: Whether the argument occured in the function call.
+     */
+    func verifyArguments<T: Equatable>(method method: String, argument: T) -> Bool {
+        guard let function = calls.filter({ $0.name == method }).first else { return false }
+        return function.verifyArgument(argument)
+    }
+    
+}
